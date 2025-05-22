f2d47543e7fe2c9fe3758c02225b6357c19d0a50
Tu Nombre <gustavosq35@gmail.com>
2025-05-22 13:55:00 +0200

    Corregida la configuración base de Vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/turnos2-app/', // Asegurarnos de que coincida con el nombre del repositorio
+  base: '/turnos-app/', // Nombre correcto del repositorio
 })
