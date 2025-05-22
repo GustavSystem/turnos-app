671e9d2def286981b9433a5f740a3acae7dd069a
Tu Nombre <gustavosq35@gmail.com>
2025-05-22 12:50:28 +0200

    fix: rutas y configuración para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/turnos2-app/', // Nombre del repositorio para GitHub Pages
+  base: '/turnos2-app/', // Asegurarnos de que coincida con el nombre del repositorio
 })
